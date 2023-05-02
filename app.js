c73254e2817ef6e57a52f25534bc2298e27ef2d5
Sebastian Perines <sebastianperinesr@gmail.com>
2023-05-01 22:56:06 -0400

    Entrega Final

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-//PRE ENTREGA 3
+//ENTREGA FINAL
 
-//pre entrega 2
-//Storage y JSON
-//Modificacion del DOM 
+//variables let/const(array) ..ok
+//estructuras de control > if/switch ..ok
+//bucles > for/forin..off..each/while do while ..ok
+//funciones > clasica o flecha ..ok
+//arrays > metodos clasicos y avanzados ..ok
+//objetos > constructor tipo class ..ok
+
+//DOM y eventos, sin alertas ni prompt ni consolas ..ok
+//eventos > click / submit > change/mouse/teclado ..ok
+//librerias > para estilos sweet alert toastify ..ok
+//sugar sintax > a nuestro criterio ..falta creo
+//fetch > json local o api de terceros ..ok
 
 //clase / objetos
 
@@ -36,23 +45,30 @@ Trabajadores.push(new Trabajador("Ian","19-12-2001","1000","Tecnico"))
 
 //variables HTML
 
-let ag= document.getElementById("agregar")
-let bu= document.getElementById("buscar")
-let el= document.getElementById("eliminar")
+let agregar= document.getElementById("agregar")
+let buscar= document.getElementById("buscar")
+let eliminar= document.getElementById("eliminar")
 
-let lt= document.getElementById("listT")
-let lj= document.getElementById("listJ")
-let le= document.getElementById("listE")
+let listarTodo= document.getElementById("listT")
+let listarJefes= document.getElementById("listJ")
+let listarEmpleados= document.getElementById("listE")
+
+let limpiarB = document.getElementById("limpiarB")
+let limpiarL = document.getElementById("limpiarL")
 
 //eventos
 
-ag.addEventListener("click", agregarT)
-bu.addEventListener("click", buscarT)
-el.addEventListener("click", eliminarT)
-lt.addEventListener("click", listarTodos)
-lj.addEventListener("click", listarJefes)
-le.addEventListener("click", listarEmp)
+agregar.addEventListener("click", agregarT)
+buscar.addEventListener("click", buscarT)
+eliminar.addEventListener("click", eliminarT)
+listarTodo.addEventListener("click", listarTodos)
+listarJefes.addEventListener("click", listaJefes)
+listarEmpleados.addEventListener("click", listarEmp)
 
+limpiarB.addEventListener("click",limpiarBusc)
+limpiarL.addEventListener("click",limpiarList)
+
+//funciones
 
 function agregarT(e) {
     e.preventDefault();
@@ -60,71 +76,125 @@ function agregarT(e) {
     let fn = Date(document.getElementById("fn").value)
     let suel = document.getElementById("s").value
     let car = document.getElementById("ca").value
-    Trabajadores.push(new Trabajador(nomb,fn,suel,car))
-    alert("Trabajador: "+nomb+" agregado")
-    actualizarStorage()
+    if (!nomb || !fn || !suel || !car) {
+        swal("Faltan datos!", "Revise el formulario!", "error");
+    } else {
+        if (buscarTrabajador(nomb)) {
+            swal("Trabajador ya existe!", "Ingrese uno nuevo!", "error");
+        } else {
+            Trabajadores.push(new Trabajador(nomb,fn,suel,car))
+            swal("Trabajador: "+nomb+" agregado!","Datos correctos", "success");
+            actualizarStorage()
+        }
+    }
 }
 
 function buscarT(e) {
     e.preventDefault();
     let nombb = document.getElementById("nb").value
-    const trab = buscarTrabajador(nombb)
-    if (trab) {
-        var padre = document.getElementById("buscarTrab")
-        var hijo1 = document.createElement("p")
-        var hijo2 = document.createElement("p")
-        var hijo3 = document.createElement("p")
-        var hijo4 = document.createElement("p")
 
-        hijo1.textContent = "Codigo: "+trab.codigo
-        hijo2.textContent = "Fecha Nacimiento: "+trab.fechnac
-        hijo3.textContent = "Sueldo: "+trab.sueldo
-        hijo4.textContent = "Es Jefe: "+trab.esJefe()
-
-        padre.appendChild(hijo1)
-        padre.appendChild(hijo2)
-        padre.appendChild(hijo3)
-        padre.appendChild(hijo4)
-
-        alert(nombb+" Encontrado")
+    if (!nombb) {
+        swal("No ingreso un nombre!", "Revise el formulario!","error");
     } else {
-        alert("No se encuentra "+nombb)
+        const trab = buscarTrabajador(nombb)
+    if (trab) {
+
+        let padre = document.getElementById("buscarTrabResultado")
+
+        padre.innerHTML = `<p>Codigo: ${trab.codigo}</p>
+        <p>Fecha Nacimiento: ${trab.fechnac}</p>
+        <p>Sueldo: $${trab.sueldo}</p>
+        <p>Es Jefe: ${trab.esJefe() ? 'SI' : 'NO'}</p>`
+
+        swal("Trabajador "+nombb+" encontrado!", "","success");
+    } else {
+        let padre = document.getElementById("buscarTrabResultado")
+
+        padre.innerHTML = ``
+
+        swal("Trabajador "+nombb+" no se encuentra!", "","error");
     }
+    }
+    
 }
 
 function eliminarT(e) {
     e.preventDefault();
     let nombe = document.getElementById("ne").value
-    const trab = buscarTrabajador(nombe)
-    var indice = Trabajadores.indexOf(trab);
-    if (trab) {
-        Trabajadores.splice(indice,1)
-        alert("Eliminado")
-        actualizarStorage()
+    if (!nombe) {
+        swal("Ingrese un nombre!", "","error");
     } else {
-        alert("No se encuentra "+ nombe)
+        const trab = buscarTrabajador(nombe)
+        var indice = Trabajadores.indexOf(trab);
+        if (trab) {
+            Trabajadores.splice(indice,1)
+            swal("Trabajador eliminado!", "","success");
+            actualizarStorage()
+        } else {
+            swal(nombe+" no se encuentra", "","error");
+        }
+    }
+    
+}
+
+function listarTodos(e) {
+    e.preventDefault();
+    
+    let padre = document.getElementById("lista")
+
+    padre.innerHTML=``
+
+    for (const data of listarT()) {
+        let hijo0 = document.createElement("p")
+        let hijo1 = document.createElement("p")
+        let hijo2 = document.createElement("p")
+        let hijo3 = document.createElement("p")
+        let hijo4 = document.createElement("p")
+        let hijo5 = document.createElement("p")
+        let hijo6 = document.createElement("p")
+
+        hijo0.textContent = "Nombre: "+data.nombre
+        hijo1.textContent = "Codigo: "+data.codigo
+        hijo2.textContent = "Fecha Nacimiento: "+data.fechnac
+        hijo3.textContent = "Sueldo: $"+data.sueldo
+        hijo4.textContent = "Cargo: "+data.cargo
+        hijo5.textContent = data.esJefe() ? "Es jefe: SI":"Es jefe: NO"
+        hijo6.textContent = "*****************************"
+
+        padre.appendChild(hijo0)
+        padre.appendChild(hijo1)
+        padre.appendChild(hijo2)
+        padre.appendChild(hijo3)
+        padre.appendChild(hijo4)
+        padre.appendChild(hijo5)
+        padre.appendChild(hijo6)
     }
 }
 
- function listarTodos(e) {
-     e.preventDefault();
+function listaJefes(e) {
+    e.preventDefault();
 
-     for (const data of listarT()) {
+    let padre = document.getElementById("lista")
 
-        var padre = document.getElementById("listas")
+    padre.innerHTML=``
+
+    for (const data of filtrarJefes()) {
+
         var hijo0 = document.createElement("p")
         var hijo1 = document.createElement("p")
         var hijo2 = document.createElement("p")
         var hijo3 = document.createElement("p")
         var hijo4 = document.createElement("p")
         var hijo5 = document.createElement("p")
+        let hijo6 = document.createElement("p")
 
         hijo0.textContent = "Nombre: "+data.nombre
         hijo1.textContent = "Codigo: "+data.codigo
         hijo2.textContent = "Fecha Nacimiento: "+data.fechnac
         hijo3.textContent = "Sueldo: "+data.sueldo
-        hijo4.textContent = "Es Jefe: "+data.cargo
-        hijo5.textContent = "Es Jefe: "+data.esJefe()
+        hijo4.textContent = "Cargo: "+data.cargo
+        hijo5.textContent = data.esJefe() ? "Es Jefe: SI":"Es Jefe: NO"
+        hijo6.textContent = "*****************************"
 
         padre.appendChild(hijo0)
         padre.appendChild(hijo1)
@@ -132,28 +202,34 @@ function eliminarT(e) {
         padre.appendChild(hijo3)
         padre.appendChild(hijo4)
         padre.appendChild(hijo5)
-     }
- }
+        padre.appendChild(hijo6)
+    }
+}
 
- function listarJefes(e) {
-     e.preventDefault();
+function listarEmp(e) {
+    e.preventDefault();
 
-     for (const data of filtrarJefes()) {
+    let padre = document.getElementById("lista")
 
-        var padre = document.getElementById("listas")
+    padre.innerHTML=``
+
+    for (const data of filtrarNoJefes()) {
+
         var hijo0 = document.createElement("p")
         var hijo1 = document.createElement("p")
         var hijo2 = document.createElement("p")
         var hijo3 = document.createElement("p")
         var hijo4 = document.createElement("p")
         var hijo5 = document.createElement("p")
+        let hijo6 = document.createElement("p")
 
         hijo0.textContent = "Nombre: "+data.nombre
         hijo1.textContent = "Codigo: "+data.codigo
         hijo2.textContent = "Fecha Nacimiento: "+data.fechnac
         hijo3.textContent = "Sueldo: "+data.sueldo
-        hijo4.textContent = "Es Jefe: "+data.cargo
-        hijo5.textContent = "Es Jefe: "+data.esJefe()
+        hijo4.textContent = "Cargo: "+data.cargo
+        hijo5.textContent = data.esJefe() ? "Es Jefe: SI":"Es Jefe: NO"
+        hijo6.textContent = "*****************************"
 
         padre.appendChild(hijo0)
         padre.appendChild(hijo1)
@@ -161,45 +237,27 @@ function eliminarT(e) {
         padre.appendChild(hijo3)
         padre.appendChild(hijo4)
         padre.appendChild(hijo5)
-     }
- }
+        padre.appendChild(hijo6)
+    }
+}
 
- function listarEmp(e) {
-     e.preventDefault();
+function limpiarBusc() {
+    let padre = document.getElementById("buscarTrabResultado")
+    padre.innerHTML = ``
+}
 
-     for (const data of filtrarNoJefes()) {
+function limpiarList() {
+    let padre = document.getElementById("lista")
 
-        var padre = document.getElementById("listas")
-        var hijo0 = document.createElement("p")
-        var hijo1 = document.createElement("p")
-        var hijo2 = document.createElement("p")
-        var hijo3 = document.createElement("p")
-        var hijo4 = document.createElement("p")
-        var hijo5 = document.createElement("p")
-
-        hijo0.textContent = "Nombre: "+data.nombre
-        hijo1.textContent = "Codigo: "+data.codigo
-        hijo2.textContent = "Fecha Nacimiento: "+data.fechnac
-        hijo3.textContent = "Sueldo: "+data.sueldo
-        hijo4.textContent = "Es Jefe: "+data.cargo
-        hijo5.textContent = "Es Jefe: "+data.esJefe()
-
-        padre.appendChild(hijo0)
-        padre.appendChild(hijo1)
-        padre.appendChild(hijo2)
-        padre.appendChild(hijo3)
-        padre.appendChild(hijo4)
-        padre.appendChild(hijo5)
-     }
- }
+    padre.innerHTML=``
+}
  
-//funciones
-//busqueda
+//funciones de busqueda
 function buscarTrabajador(nombreT) {
     return Trabajadores.find(dato => dato.nombre === nombreT)
 }
 
-//filtrado los que no son jefes
+//funciones de filtrado
 function filtrarNoJefes() {
     return Trabajadores.filter(dato => dato.cargo != "Jefe")
 }
@@ -213,7 +271,6 @@ function listarT() {
 }
 
 //STORAGE/JSON
-//const storageT = localStorage.setItem("trabajadores",JSON.stringify(Trabajadores))
 
 function actualizarStorage() {
     localStorage.clear()
@@ -223,7 +280,14 @@ function actualizarStorage() {
     }
 }
 
+//FETCH API de chuck norris
 
-//console.log(localStorage.getItem(1))
-
-//console.log(listarT())
+fetch('https://api.chucknorris.io/jokes/random?category=dev')
+.then(response => response.json())
+.then(data => {
+    const chuck = `${data.value}`;
+    document.getElementById('apichuck').textContent = chuck;
+})
+.catch(error => {
+    console.error('Error api:', error);
+});
